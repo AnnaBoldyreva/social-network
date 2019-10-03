6eb09f1522e94d25f6da23752bbec8b47d75d9ff
AnnaBoldyreva <annboldyrevaa@gmail.com>
2019-10-03 18:54:06 +0300

    add grid styles to Navbar + add list of navigation items

--- src/Navbar/Navbar.js
+++ src/Navbar/Navbar.js
@@ -4,8 +4,12 @@ import styles from './Navbar.module.css'
 
 const Navbar = () => {
   return (
-    <div className={styles.navbar}>
-    kkkk
+    <div className={styles.navigation}>
+    <div>Profile</div>
+      <div>Messages</div>
+      <div>News</div>
+      <div>Music</div>
+      <div>Settings</div>
     </div>
   );
 }
