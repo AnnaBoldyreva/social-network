6cab98ad15f348c4f321b1dd5b38bb85cc8739a3
AnnaBoldyreva <annboldyrevaa@gmail.com>
2019-10-13 14:28:41 +0300

    handle event Onclick, now we can read Data from UI by using input

--- src/DialogsPage/MessagePage/Message.js
+++ src/DialogsPage/MessagePage/Message.js
@@ -4,9 +4,22 @@ import styles from './Message.module.css';
 
 
 const Message = (props) => {
+
+  let messageEl = React.createRef();
+
+  let sendMessage = () => {
+    let text = messageEl.current.value
+    alert(text)
+  };
+
   return (
 
-        <div className={styles.message}>{props.message}</div>
+        <div className={styles.message}>{props.message}
+        <br/>
+          <textarea  ref={messageEl}  placeholder='type new message...'> </textarea>
+          <button onClick={sendMessage}>Send</button>
+        </div>
+
 
   );
 };
