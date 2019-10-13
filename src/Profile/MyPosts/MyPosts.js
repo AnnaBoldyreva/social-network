b50200544e45faffb67f5b0e8f4fa1b6096205f0
AnnaBoldyreva <annboldyrevaa@gmail.com>
2019-10-13 14:15:18 +0300

    create ref, give it to textarea, after give ref to addPost function

--- src/Profile/MyPosts/MyPosts.js
+++ src/Profile/MyPosts/MyPosts.js
@@ -7,16 +7,19 @@ const MyPosts = (props) => {
 
     let postsElement = props.postsData.map(post => <Post message={post.message} likeCount={post.likeCount}/>);
 
+    let newPostElement = React.createRef();
+
     let addPost = () => {
-        alert('samurai')
+        let text = newPostElement.current.value;
+        alert(text)
     };
 
     return (
         <div className={styles.postsWrapper}>
             <h2>My new Post</h2>
             <div>
-            <textarea placeholder='type something...'>
-            </textarea>
+            <textarea  ref={newPostElement} placeholder='type something...'>
+            </textarea >
                 <div>
                     <button onClick={addPost}>post</button>
                 </div>
