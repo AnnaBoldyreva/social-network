b06dbb4b4d2780c7285d7140ac5eec49105444ab
AnnaBoldyreva <annboldyrevaa@gmail.com>
2019-11-03 15:35:55 +0300

    refactoring dialogs-reducer

--- src/Redux/dialogs-reducer.js
+++ src/Redux/dialogs-reducer.js
@@ -3,18 +3,22 @@
     const UPDATE_NEW_MESSAGE_TEXT = 'UPDATE_NEW_MESSAGE_TEXT';
 
 
-    if (action.type === SEND_MESSAGE){
-        let newMessage = {
-            id: 6,
-            message: state.newMessageText
-        };
-        state.messagesData.push(newMessage);
-        state.newMessageText = '';
-    } else
-    if(action.type === UPDATE_NEW_MESSAGE_TEXT){
-        state.newMessageText = action.newText;
+    switch (action.type){
+        case SEND_MESSAGE:
+            let newMessage = {
+                id: 6,
+                message: state.newMessageText
+            };
+            state.messagesData.push(newMessage);
+            state.newMessageText = '';
+            return state;
+        case UPDATE_NEW_MESSAGE_TEXT:
+            state.newMessageText = action.newText;
+            return state;
+        default:
+            return state;
+
     }
-    return state;
 };
 
 export default dialogsReducer;
