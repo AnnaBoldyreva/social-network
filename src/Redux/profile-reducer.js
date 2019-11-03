6dd446ec097de7f77a0c1571a8aea776a0033a43
AnnaBoldyreva <annboldyrevaa@gmail.com>
2019-11-03 15:34:06 +0300

    refactoring in profile-reducer

--- src/Redux/profile-reducer.js
+++ src/Redux/profile-reducer.js
@@ -3,20 +3,22 @@
     const UPDATE_NEW_POST_TEXT = 'UPDATE-NEW-POST-TEXT';
 
 
-    if (action.type === ADD_POST){
-        let newPost = {
-            id: 5,
-            message: this._state.profilePage.newPostText,
-            likeCount: 0
-        };
-        state.postsData.push(newPost);
-        state.newPostText = '';
-    } else
-    if (action.type === UPDATE_NEW_POST_TEXT ){
-        state.newPostText= action.newText;
+    switch (action.type) {
+        case ADD_POST:
+            let newPost = {
+                id: 5,
+                message: state.newPostText,
+                likeCount: 0
+            };
+            state.postsData.push(newPost);
+            state.newPostText = '';
+            return state;
+        case UPDATE_NEW_POST_TEXT:
+            state.newPostText= action.newText;
+            return state;
+        default:
+            return state;
     }
-
-    return state;
 };
 
    export default profileReducer;
