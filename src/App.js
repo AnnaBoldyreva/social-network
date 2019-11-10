5bb6a5d9de60b6661c1e32fe5516470a5e0a4508
AnnaBoldyreva <annboldyrevaa@gmail.com>
2019-11-10 17:23:25 +0300

    refactoring

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import './App.css';
 import Header from "./Header/Header";
 import Navbar from "./Navbar/Navbar";
 import Profile from "./Profile/Profile";
-import DialogsPage from "./DialogsPage/DialogsPage";
 import News from "./News/News";
 import MusicPage from "./MusicPage/MusicPage";
 import SettingsPage from "./SettingsPage/SettingsPage";
