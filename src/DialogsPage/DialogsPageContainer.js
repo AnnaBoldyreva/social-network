d6fcbd8d9b91e133b10ad99dfb1c552ab9fe1176
AnnaBoldyreva <annboldyrevaa@gmail.com>
2019-11-15 21:02:50 +0300

    create connect by react-redux

--- src/DialogsPage/DialogsPageContainer.js
+++ src/DialogsPage/DialogsPageContainer.js
@@ -1,24 +1,30 @@
 import React from 'react';
 import {sendMessageActionCreator, updateNewMessageTextActionCreator} from "../Redux/dialogs-reducer";
 import DialogsPage from "./DialogsPage";
+import {connect} from "react-redux";
 
 
-const DialogsPageContainer = (props) => {
 
-
-    let onMessageClick = () => {
-        props.store.dispatch(sendMessageActionCreator())
-    };
-
-    let onNewMessageChange = (body) => {
-        props.store.dispatch(updateNewMessageTextActionCreator(body));
-
-    };
-
-    return (
-    <DialogsPage store={props.store} updateNewMessageBody={onNewMessageChange} sendMessage={onMessageClick}/>
-    )
+let mapStateToProps = (state) => {
+    return{
+        dialogsData: state.dialogsData
+    }
 };
+
+let mapDispatchToProps = (dispatch) => {
+    return {
+        updateNewMessageBody: (body) => {
+           dispatch(updateNewMessageTextActionCreator(body));
+        },
+        sendMessage:  () => {
+            dispatch(sendMessageActionCreator())
+        }
+    }
+};
+
+
+const DialogsPageContainer = connect (mapStateToProps,mapDispatchToProps)(DialogsPage);
+
 
 
 export default DialogsPageContainer;
